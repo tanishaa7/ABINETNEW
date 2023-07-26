63df759cafe58121defee39f91acec340c3cedf2
tanishaa7 <tanishapal2001@gmail.com>
2023-07-26 22:04:41 +0530

    Initial commit

---
+++ ABINetNEW_copy/batch_run.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --config=configs/pretrain_vision_model_epoch0.yaml > epoch1.txt
+#cp pretrain-vision-model_0_11000.pth 1.pth
+#CUDA_VISIBLE_DEVICES=0 python main.py --config=configs/pretrain_vision_model_epoch1.yaml > epoch2.txt
+#cp /ssd_scratch/cvit/tanisha/ABINetNEW/workdir/pretrain-vision-model/pretrain-vision-model_0_11000.pth /ssd_scratch/cvit/tanisha/ABINetNEW/workdir/pretrain-vision-model/2.pth
+#CUDA_VISIBLE_DEVICES=0 python main.py --config=configs/pretrain_vision_model_epoch2.yaml 
